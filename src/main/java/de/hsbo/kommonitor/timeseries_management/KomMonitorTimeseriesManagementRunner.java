30879181f0cb83ba27cb98f525323b8fd5f683ac
bpross-52n <b.pross@52north.org>
2026-02-13 11:09:29 +0100

    Enable CORS

--- src/main/java/de/hsbo/kommonitor/timeseries_management/KomMonitorTimeseriesManagementRunner.java
+++ src/main/java/de/hsbo/kommonitor/timeseries_management/KomMonitorTimeseriesManagementRunner.java
@@ -1,10 +1,14 @@
 package de.hsbo.kommonitor.timeseries_management;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @SpringBootApplication()
@@ -12,7 +16,20 @@ import org.springframework.context.annotation.ComponentScan;
 @EntityScan( basePackages = {"de.hsbo.kommonitor"} )
 public class KomMonitorTimeseriesManagementRunner extends SpringBootServletInitializer{
 
+	@Value( "${kommonitor.timeseries-management.allowed-cors-origins}" )
+	private String allowedOrigins;
+	
     public static void main(String[] args) {
         SpringApplication.run(KomMonitorTimeseriesManagementRunner.class, args);
     }
+    
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(allowedOrigins);
+			}
+		};
+	}
 }
